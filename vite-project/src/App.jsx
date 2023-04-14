b27c225f3e6651527dc7c54c26d0adac94a6e579
steffen kroyer <stef5450@stud.kea.dk>
2023-04-14 12:14:27 +0200

    added som css the the buttons

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [masterCount, setMasterCount] = useState(0);
@@ -24,3 +25,13 @@ function Child({ masterCount }) {
 }
 
 export default App;
+
+/*
+This is a simple React application that consists of an App component and a Child component.
+
+The App component renders a button that displays the value of the masterCount state variable, which is initialized to 0 using the useState hook. When the button is clicked, the updateLocalCount function is called, which increments the value of masterCount by 1 using the setMasterCount function.
+
+The App component also renders two instances of the Child component, passing the masterCount state variable as a prop to each instance. The Child component also uses the useState hook to initialize a childCount state variable to 0, and renders a button that displays the sum of masterCount and childCount. When the button is clicked, the updateChildCount function is called, which increments the value of childCount by 1 using the setChildCount function.
+
+Overall, this application demonstrates how state can be managed and passed between parent and child components in a React application using the useState hook and props.
+ */
